acad6852f55695d383eac91d76d658a3c9fbb842
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-17 17:13:14 +0900

    [스프링핵심원리-기본편] 20240517 chapter06. 컴포넌트스캔

--- src/main/java/hello/core/member/MemoryMemberRepository.java
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -1,8 +1,11 @@
 package hello.core.member;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+@Component
 public class MemoryMemberRepository implements MemberRepository {
 
     private static final Map<Long, Member> storage = new ConcurrentHashMap<>();

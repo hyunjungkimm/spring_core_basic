b33bfb929f5d9b6305e1058cb9de264cfe97f415
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-18 16:23:20 +0900

    [스프링핵심원리-기본편] 20240517 chapter08. 의존관계자동주입

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,7 +1,5 @@
 package hello.core;
 
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -11,9 +9,4 @@ import org.springframework.context.annotation.FilterType;
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = {Configuration.class})
 )
 public class AutoAppConfig {
-
-    @Bean(name = "memoryMemberRepository")
-    public MemoryMemberRepository memoryMemberRepository() {
-        return new MemoryMemberRepository();
-    }
 }

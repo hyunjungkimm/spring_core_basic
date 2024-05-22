75fa28f837703a5139b694f6985b96f5198696ee
kimhyeonjeong <khjjung1124@gmail.com>
2024-05-22 15:18:27 +0900

    [스프링핵심원리-기본편] 20240522 chapter09 빈스코프 - 웹스코프

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -14,23 +14,23 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public MemberService memberService() {
-        System.out.println("call AppConfig.memberService");
-        return new MemberServiceImpl(memberRepository());
-    }
-    @Bean
-    public OrderService orderService() {
-        System.out.println("call AppConfig.orderService");
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
-    @Bean
-    public MemberRepository memberRepository() {
-        System.out.println("call AppConfig.memberRepository");
-        return new MemoryMemberRepository();
-    }
-    @Bean
-    public DiscountPolicy discountPolicy() {
-        return new RateDiscountPolicy();
-    }
+//    @Bean
+//    public MemberService memberService() {
+//        System.out.println("call AppConfig.memberService");
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//    @Bean
+//    public OrderService orderService() {
+//        System.out.println("call AppConfig.orderService");
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
+//    }
+//    //@Bean
+//    public MemberRepository memberRepository() {
+//        System.out.println("call AppConfig.memberRepository");
+//        return new MemoryMemberRepository();
+//    }
+//    @Bean
+//    public DiscountPolicy discountPolicy() {
+//        return new RateDiscountPolicy();
+//    }
 }
